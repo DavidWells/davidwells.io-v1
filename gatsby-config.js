28efb31310a06123c5fcc046623c94b9c3b004a7
DavidWells <hello@davidwells.io>
2019-06-02 10:41:53 -0700

    content layout!

--- gatsby-config.js
+++ gatsby-config.js
@@ -130,10 +130,10 @@ module.exports = {
               maxWidth: 960,
             },
           },
-          {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: { wrapperStyle: 'margin-bottom: 1.0725rem' },
-          },
+          // {
+          //   resolve: 'gatsby-remark-responsive-iframe',
+          //   options: { wrapperStyle: 'margin-bottom: 1.0725rem' },
+          // },
           'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
